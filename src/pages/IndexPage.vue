e71d6628d7d252ab21fed578ef11c72e56356c2e
bibibala <1140918095@qq.com>
2023-12-03 14:13:21 +0800

    Add files via upload

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+  <span class="text-primary">hello world</span>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
